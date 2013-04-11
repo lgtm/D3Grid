5ddc2dc750321374a827824bb4b841e5ff9e4058
Sam Moore <samandmoore@gmail.com>
2013-04-11 10:42:17 -0400

    cleaned up unused nuget packages

--- D3Grid.Mvc4Demo/Global.asax.cs
+++ D3Grid.Mvc4Demo/Global.asax.cs
@@ -8,18 +8,17 @@ using System.Web.Routing;
 
 namespace D3Grid.Mvc4Demo
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
 }
